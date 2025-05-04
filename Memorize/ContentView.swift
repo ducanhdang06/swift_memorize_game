471912a9f963f8b59c0faa7d2c29e52c831b6807
Duc Anh Dang <danganhbill0806@gmail.com>
2025-05-03 21:37:59 -0500

    Inital Commit CardView isFaceUp

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Anh Dang on 5/3/25.
+//
+
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView : View {
+    var isFaceUp : Bool = false // arguments
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("🤓").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Anh Dang on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
