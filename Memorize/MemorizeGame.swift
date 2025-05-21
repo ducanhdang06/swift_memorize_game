b2ba3a8b93e59b5b3f784e924359e62462819f9f
Duc Anh Dang <danganhbill0806@gmail.com>
2025-05-21 08:10:43 +0700

    Add backend features

---
+++ Memorize/MemorizeGame.swift
@@ -0,0 +1,36 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Anh Dang on 5/10/25.
+//
+// model
+import Foundation
+
+struct MemoryGame <CardContent> {
+    
+    // the only struct that can make changes to this cards is this struct
+    // but it allows others to see (not set)
+    private(set) var cards: Array<Card>
+    
+    // initializer
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent){
+        cards = []
+        // add number of pairs of cards x 2 cards
+        for pairIndex in 0..<max(2,numberOfPairsOfCards) {
+            let content : CardContent = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    func choose(_ card: Card){
+        
+    }
+    
+    struct Card {
+        var isFaceUp = false
+        var isMatched = false
+        let content: CardContent // dont care types (it can be anything)
+    }
+}
